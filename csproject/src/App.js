c2e4d064ed1f570f952e501b8f7a265f4ae093ae
xinepeng <smartpeoplelosesp@gmail.com>
2024-06-04 22:50:32 -0400

    fwfwe

--- csproject/src/App.js
+++ csproject/src/App.js
@@ -1,39 +1,35 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AddProductForm from './AddProductForm';
 import ProductList from './ProductList';
-import AppHeader from './AppHeader'; // Make sure this path is correct
+import AppHeader from './AppHeader';
 import { MantineProvider } from "@mantine/core";
 
 const App = () => {
-  const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
 
-  const handleAddProduct = (product) => {
-    setProducts([...products, product]);
-  };
+    const handleAddProduct = (product) => {
+        setProducts([...products, product]);
+    };
 
-  const handleRemoveProduct = (id) => {
- setProducts(products.filter(product => product.id !== id));
-  };
+    const handleRemoveProduct = (id) => {
+        setProducts(products.filter(product => product.id !== id));
+    };
 
-  return (
-    <Router>
-      <div className="App">
-        <AppHeader /> {/* This is where you call AppHeader */}
-        <h1>Best Sellers</h1>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/add-product">Add Product</Link></li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<ProductList products={products} handleRemove={handleRemoveProduct} />} />
-          <Route path="/add-product" element={<AddProductForm handleAddProduct={handleAddProduct} />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+    return (
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+            <Router>
+                <div className="App">
+                    <AppHeader />
+                    <h1>Best Sellers</h1>
+                    <Routes>
+                        <Route path="/" element={<ProductList products={products} handleRemove={handleRemoveProduct} />} />
+                        <Route path="/add-product" element={<AddProductForm handleAddProduct={handleAddProduct} />} />
+                    </Routes>
+                </div>
+            </Router>
+        </MantineProvider>
+    );
 }
 
 export default App;
--- csproject/src/AppHeader.js
+++ csproject/src/AppHeader.js
@@ -1,22 +1,20 @@
-import { Header, Group, Button } from '@mantine/core';
+import React from 'react';
+import { Title, Group, Anchor, Flex, Divider } from '@mantine/core';
 import { Link } from 'react-router-dom';
 
 const AppHeader = () => {
-  return (
-    <Header height={60} p="xs">
-      <Group position="apart" style={{ height: '100%' }}>
-        <div>Best Sellers</div>
-        <Group>
-          <Button variant="subtle" component={Link} to="/">
-            Home
-          </Button>
-          <Button variant="subtle" component={Link} to="/add-product">
-            Add Product
-          </Button>
-        </Group>
-      </Group>
-    </Header>
-  );
+    return (
+        <header>
+            <Flex justify="space-between" align="center" style={{ padding: '0 20px' }}>
+                <Title order={1}>HolmerMarkets</Title>
+                <Group spacing="xl">
+                    <Anchor component={Link} to="/" underline={false}>Home</Anchor>
+                    <Anchor component={Link} to="/add-product" underline={false}>Add Product</Anchor>
+                </Group>
+            </Flex>
+            <Divider />
+        </header>
+    );
 };
 
 export default AppHeader;
